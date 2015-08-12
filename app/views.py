214e516fc548aaf3d506b0a64fab6614b1fb1606
JoelSpeed <Joel.speed@hotmail.co.uk>
2015-08-12 07:19:22 +0100

    Introduce page as a variable storage
    page can now be used as a dictionary to store page by page variable eg.
    titles, paths, authentication level required etc etc

--- app/views.py
+++ app/views.py
@@ -6,9 +6,9 @@ from app import app
 @app.route('/index')
 def index():
     #Load index page and pass app object
-    return render_template('pages/index.html', app=app, filmsoc=filmsoc)
+    return render_template('pages/index.html', path = '/', filmsoc=filmsoc)
 
 @app.route('/<path:path>')
 def page(path):
     #Load any unfiltered page by searching it's path, passing app object
-    return render_template('pages/'+path+'.html', app=app, filmsoc=filmsoc)
+    return render_template('pages/'+path+'.html', path=path , filmsoc=filmsoc)
