852861e84ff37686f1a13a9f2bb7420972c37da3
JoelSpeed <Joel.speed@hotmail.co.uk>
2015-08-16 18:27:49 +0100

    Set up as VS project
    Configured a Visual Studio project file, in case anyone wants to use VS
    community. Also added some CSS, because why not!

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
+﻿"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
 from flask import render_template
 import filmsoc
 from app import app
@@ -5,10 +10,10 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    #Load index page and pass app object
-    return render_template('pages/index.html', path = '/', filmsoc=filmsoc)
+    # Load index page and pass app object
+    return render_template('pages/index.html', path = '/', filmsoc=filmsoc, year=datetime.now().year)
 
 @app.route('/<path:path>')
 def page(path):
-    #Load any unfiltered page by searching it's path, passing app object
-    return render_template('pages/'+path+'.html', path=path , filmsoc=filmsoc)
+    # Load any unfiltered page by searching it's path, passing app object
+    return render_template('pages/'+path+'.html', path=path , filmsoc=filmsoc, year=datetime.now().year)
