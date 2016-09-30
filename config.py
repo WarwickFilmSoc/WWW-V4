ffff03bc41d0c4bc13a8bc6484c3f369e26c1e90
Joel Speed <Joel.speed@hotmail.co.uk>
2016-09-30 21:12:02 +0100

    Missing import of OS

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config(object):
     # Where to find the templates to build the pages from
     TEMPLATE_ROOT = 'templates/default/' # folder containing current template relative to app folder
