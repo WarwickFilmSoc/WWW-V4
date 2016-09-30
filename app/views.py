6a7d254a1cc31a47657f2df1c67f7390c15119f6
Joel Speed <Joel.speed@hotmail.co.uk>
2016-09-30 20:40:10 +0100

    Update Jinja globals
    Don't need to pass these as variables anymore

--- app/views.py
+++ app/views.py
@@ -7,13 +7,16 @@ from flask import render_template
 import filmsoc
 from app import app
 
+app.jinja_env.globals['filmsoc'] = filmsoc
+app.jinja_env.globals['app'] = app
+app.jinja_env.globals['year'] = year=datetime.now().year
 @app.route('/')
 @app.route('/index')
 def index():
     # Load index page and pass app object
-    return render_template('pages/index.html', path = '/', filmsoc=filmsoc, year=datetime.now().year)
+    return render_template('pages/index.html', path = '/')
 
 @app.route('/<path:path>')
 def page(path):
     # Load any unfiltered page by searching it's path, passing app object
-    return render_template('pages/'+path+'.html', path=path , filmsoc=filmsoc, year=datetime.now().year)
+    return render_template('pages/'+path+'.html', path=path)
